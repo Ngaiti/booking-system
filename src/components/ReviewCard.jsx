fe4dd4bd18be30e59a056f3124873754b99b2e57
Ngaiti <henfanar@gmail.com>
2023-07-27 17:11:55 +0800

    Added review cards

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react"
+import { Modal, Form, Button } from "react-bootstrap";
+
+
+export default function ReviewCard({ show, onClose, onSave, selectedReview, newReview, handleChange }) {
+    return (
+        <Modal show={show} onHide={onClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>{selectedReview ? 'Edit Review' : 'Create New Review'}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Group controlId="formTitle">
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="title"
+                            value={newReview.title}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="formAuthor">
+                        <Form.Label>Author</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="author"
+                            value={newReview.author}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="formDate">
+                        <Form.Label>Date</Form.Label>
+                        <Form.Control
+                            type="date"
+                            name="date"
+                            value={newReview.date}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="formContent">
+                        <Form.Label>Content</Form.Label>
+                        <Form.Control
+                            as="textarea"
+                            rows={4}
+                            name="content"
+                            value={newReview.content}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="formRating">
+                        <Form.Label>Rating</Form.Label>
+                        <Form.Control
+                            type="number"
+                            name="rating"
+                            value={newReview.rating}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="btn btn-outline-secondary" onClick={onClose}>
+                    Cancel
+                </Button>
+                <Button variant="outline-primary" onClick={selectedReview ? onSave : onSave}>
+                    {selectedReview ? 'Update Review' : 'Create Review'}
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Container, Table, Button, Modal, Form } from 'react-bootstrap';
+import { Container, Button, Card, ButtonGroup, Row, Col } from 'react-bootstrap';
 import { auth } from '../firebase';
 import { useNavigate } from 'react-router-dom';
+import ReviewCard from '../components/ReviewCard';
 
-const API_URL = 'https://booking-system-api-ngaiti.sigma-school-full-stack.repl.co/bookings';
+const API_URL = 'https://capstone-project.ngaiti.repl.co/reviews';
 
 const Home = () => {
 
@@ -25,223 +26,165 @@ const Home = () => {
     }, [navigate]);
 
 
-
-    const [bookings, setBookings] = useState([]);
-    const [showModal, setShowModal] = useState(false);
-    const [newBooking, setNewBooking] = useState({
+    const [showUpdateModal, setShowUpdateModal] = useState(false);
+    const [showCreateModal, setShowCreateModal] = useState(false);
+    const [reviews, setReviews] = useState([]);
+    // const [showModal, setShowModal] = useState(false);
+    const [newReview, setNewReview] = useState({
         title: '',
-        description: '',
+        author: '',
         date: '',
-        time: '',
-        phone_number: '',
-        email: '',
-        user_id: ''
+        content: '',
+        rating: ''
     });
-    const [selectedBooking, setSelectedBooking] = useState(null);
+    const [selectedReview, setSelectedReview] = useState(null);
 
 
-    const handleOpenModal = (booking) => {
-        setShowModal(true);
-        if (booking) {
-            setSelectedBooking(booking);
-            setNewBooking({
-                title: booking.title,
-                description: booking.description,
-                date: booking.date,
-                time: booking.time,
-                phone_number: booking.phone_number,
-                email: booking.email,
-                user_id: booking.user_id,
+    const handleOpenModal = (review) => {
+        if (review) {
+            setShowUpdateModal(true);
+            setSelectedReview(review);
+            setNewReview({
+                title: review.title,
+                author: review.author,
+                date: review.date,
+                content: review.content,
+                rating: review.rating,
             });
         } else {
-            setSelectedBooking(null);
-            setNewBooking({
+            setShowCreateModal(true);
+            setSelectedReview(null);
+            setNewReview({
                 title: '',
-                description: '',
+                author: '',
                 date: '',
-                time: '',
-                phone_number: '',
-                email: '',
-                user_id: '',
+                content: '',
+                rating: ''
             });
         }
     };
 
     const handleCloseModal = () => {
-        setShowModal(false);
-        // setSelectedBooking(null);
-        setNewBooking({
+        setShowCreateModal(false);
+        setShowUpdateModal(false);
+        setNewReview({
             title: '',
-            description: '',
+            author: '',
             date: '',
-            time: '',
-            phone_number: '',
-            email: '',
-            user_id: ''
+            content: '',
+            rating: ''
         });
+    };
+
+    const handleCloseUpdateModal = () => {
+        setShowUpdateModal(false);
     };
 
 
     // Fetch all bookings
-    const fetchBookings = async () => {
+    const fetchReviews = async () => {
         try {
             const response = await axios.get(API_URL);
-            setBookings(response.data.data);
+            setReviews(response.data.data);
         } catch (error) {
             console.error('Error:', error.message);
         }
     };
 
-    // Create new booking
-    const createBooking = async () => {
+    // Create new review
+    const createReview = async () => {
         try {
-            const response = await axios.post(API_URL, newBooking);
-            const createdBooking = response.data.data;
-            setBookings([...bookings, createdBooking]);
+            const response = await axios.post(API_URL, newReview);
+            const createdReview = response.data.data;
+            setReviews([...reviews, createdReview]);
             handleCloseModal();
         } catch (error) {
             console.error('Error:', error.message);
         }
     };
 
-    //Update booking
-    const updateBooking = async () => {
+    const updateReview = async () => {
         try {
-            if (!selectedBooking) {
-                console.error('Error updating booking: No booking selected.');
-                return;
-            }
-            const response = await axios.put(`${API_URL}/${selectedBooking.booking_id}`, newBooking);
-            const updatedBooking = response.data && response.data.data;
-            setBookings((prevBookings) =>
-                prevBookings.map((booking) =>
-                    booking.booking_id === updatedBooking.booking_id
-                )
+            await axios.put(`${API_URL}/${selectedReview.id}`, newReview);
+            const updatedReviews = reviews.map((review) =>
+                review.id === selectedReview.id ? { ...review, ...newReview } : review
             );
-            handleCloseModal();
+            setReviews(updatedReviews);
+            handleCloseUpdateModal();
         } catch (error) {
-            console.error('Error updating booking:', error.message);
+            console.error('Error:', error.message);
         }
     };
 
 
-    // Delete a booking
-    const deleteBooking = async (bookingId) => {
-        const confirmDelete = window.confirm("Are you sure you want to delete this booking?");
+    const deleteReview = async (id) => {
+        const confirmDelete = window.confirm("Are you sure you want to delete this review?");
         if (confirmDelete) {
             try {
-                await axios.delete(`${API_URL}/${bookingId}`);
-                setBookings(bookings.filter((booking) => booking.booking_id !== bookingId));
+                await axios.delete(`${API_URL}/${id}`);
+                const updatedReviews = reviews.filter((review) => review.id !== id);
+                setReviews(updatedReviews);
             } catch (error) {
                 console.error('Error:', error.message);
             }
         }
-    };
-
-
+    }
 
 
     const handleChange = (e) => {
         const { name, value } = e.target;
-        setNewBooking((prevBooking) => ({ ...prevBooking, [name]: value }));
+        setNewReview((prevReview) => ({ ...prevReview, [name]: value }));
     };
 
     useEffect(() => {
-        fetchBookings();
+        fetchReviews();
     }, []);
 
     if (loading) {
         return <div>Loading...</div>;
     }
     return (
-        <Container className="mt-5">
-            <h1 className="mb-4">Booking App</h1>
-            <Button variant="btn btn-outline-primary" onClick={() => handleOpenModal(null)} className="mb-3">
-                Create Booking
+        <div>
+            <h1>Reviews</h1>
+            <Button variant="primary" onClick={() => handleOpenModal(null)}>
+                Create New Review
             </Button>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>Booking ID</th>
-                        <th>Title</th>
-                        <th>Description</th>
-                        <th>Date</th>
-                        <th>Time</th>
-                        <th>Phone Number</th>
-                        <th>Email</th>
-                        <th>User ID</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {bookings.map((booking) => (
-                        <tr key={booking.booking_id}>
-                            <td>{booking.booking_id}</td>
-                            <td>{booking.title}</td>
-                            <td>{booking.description}</td>
-                            <td>{booking.date}</td>
-                            <td>{booking.time}</td>
-                            <td>{booking.phone_number}</td>
-                            <td>{booking.email}</td>
-                            <td>{booking.user_id}</td>
-                            <td>
-                                <Button variant="btn btn-outline-primary mx-4" className='m-3' onClick={() => handleOpenModal(booking)}>
-                                    Edit
-                                </Button>
-                                <Button variant="btn btn-outline-danger" onClick={() => deleteBooking(booking.booking_id)}>
-                                    Delete
-                                </Button>
-                            </td>
-                        </tr>
+            <Container>
+                <Row xs={1} md={2} lg={3} className="g-4">
+                    {reviews.map((review, index) => (
+                        <Col key={index}>
+                            <Card className="my-3 border-2">
+                                <Card.Body>
+                                    <Card.Title>{review.title}</Card.Title>
+                                    <Card.Text>{review.content}</Card.Text>
+                                    <div>
+                                        <strong>Rating:</strong> {review.rating} out of 10
+                                    </div>
+                                    <div className="d-flex justify-content-center align-items-center">
+                                        <ButtonGroup aria-label="Basic example">
+                                            <Button variant="secondary" onClick={() => handleOpenModal(review)}>
+                                                <i className="bi bi-pencil"></i>
+                                            </Button>
+                                            <Button variant="secondary" onClick={() => deleteReview(review.id)}>
+                                                <i className="bi bi-trash3"></i>
+                                            </Button>
+                                        </ButtonGroup>
+                                    </div>
+                                </Card.Body>
+                            </Card>
+                        </Col>
                     ))}
-                </tbody>
-            </Table>
-            <Modal show={showModal} onHide={handleCloseModal}>
-                <Modal.Header closeButton>
-                    <Modal.Title>{selectedBooking ? 'Edit Booking' : 'Create Booking'}</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form>
-                        <Form.Group controlId="formTitle">
-                            <Form.Label>Title</Form.Label>
-                            <Form.Control type="text" name="title" value={newBooking.title} onChange={handleChange} />
-                        </Form.Group>
-                        <Form.Group controlId="formDescription">
-                            <Form.Label>Description</Form.Label>
-                            <Form.Control as="textarea" rows={3} name="description" value={newBooking.description} onChange={handleChange} />
-                        </Form.Group>
-                        <Form.Group controlId="formDate">
-                            <Form.Label>Date</Form.Label>
-                            <Form.Control type="date" name="date" value={newBooking.date} onChange={handleChange} />
-                        </Form.Group>
-                        <Form.Group controlId="formTime">
-                            <Form.Label>Time</Form.Label>
-                            <Form.Control type="time" name="time" value={newBooking.time} onChange={handleChange} />
-                        </Form.Group>
-                        <Form.Group controlId="formPhoneNumber">
-                            <Form.Label>Phone Number</Form.Label>
-                            <Form.Control type="tel" name="phone_number" value={newBooking.phone_number} onChange={handleChange} />
-                        </Form.Group>
-                        <Form.Group controlId="formEmail">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" name="email" value={newBooking.email} onChange={handleChange} />
-                        </Form.Group>
-                        <Form.Group controlId="formUserId">
-                            <Form.Label>User ID</Form.Label>
-                            <Form.Control type="text" name="user_id" value={newBooking.user_id} onChange={handleChange} />
-                        </Form.Group>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="btn btn-outline-secondary" onClick={handleCloseModal}>
-                        Cancel
-                    </Button>
-                    <Button variant="btn btn-outline-primary" onClick={selectedBooking ? updateBooking : createBooking}>
-                        {selectedBooking ? 'Update Booking' : 'Create Booking'}
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </Container>
+                </Row>
+            </Container>
+            <ReviewCard
+                show={showCreateModal || showUpdateModal}
+                onClose={handleCloseModal}
+                onSave={selectedReview ? updateReview : createReview}
+                selectedReview={selectedReview}
+                newReview={newReview}
+                handleChange={handleChange}
+            />
+        </div>
     );
 };
 
